efddad7939641b1782272d20925fc9ceccd47e57
Rajitha Udawalpola <rajitha@eie.ruh.ac.lk>
2024-03-06 22:46:24 +0530

    ui2

--- ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
+++ ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ac35a40c971e7b764e452a90e1ac9fc05c9f0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8c87e6643f34eb02dc0f8dd6fd0e7fa5dc4518")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
