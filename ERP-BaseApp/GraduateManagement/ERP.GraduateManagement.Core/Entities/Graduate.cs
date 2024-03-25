ecc75ddf4861784beafc2df8d7d9acacc05e7966
riyajidevindu <riyajidevindu@gmail.com>
2024-03-25 21:57:20 +0530

    Add Graduate DB sets

---
+++ ERP-BaseApp/GraduateManagement/ERP.GraduateManagement.Core/Entities/Graduate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.GraduateManagement.Core.Entities
+{
+    public class Graduate : BaseEntity
+    {
+        public string RegNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string Degree { get; set; }
+        public string? CurrentCompany { get; set; }
+        public string? CurrentJobPosition { get; set; }
+
+    }
+}
--- ERP-BaseApp/src/StudentRegistration/ERP.StudentRegistration.Core/obj/Debug/net8.0/ERP.StudentRegistration.Core.AssemblyInfo.cs
+++ ERP-BaseApp/src/StudentRegistration/ERP.StudentRegistration.Core/obj/Debug/net8.0/ERP.StudentRegistration.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.StudentRegistration.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911673b5cd13890ed3a43f84cacb75c2dab840c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ada6a5cf5462f66c5aa0c089542877ada7af71")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.StudentRegistration.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.StudentRegistration.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
