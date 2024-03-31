3304f30dcfaf0e410d7b1eac89bf58ccbdc6d82e
Sahaswari <smsahaswari@gmail.com>
2024-03-31 12:05:41 +0530

    check commit

--- ERP-BaseApp/src/ResultAnalysis/ERP.ResultAnalysis.DataService/obj/Debug/net8.0/ERP.ResultAnalysis.DataService.AssemblyInfo.cs
+++ ERP-BaseApp/src/ResultAnalysis/ERP.ResultAnalysis.DataService/obj/Debug/net8.0/ERP.ResultAnalysis.DataService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.ResultAnalysis.DataService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc75ddf4861784beafc2df8d7d9acacc05e7966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.ResultAnalysis.DataService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.ResultAnalysis.DataService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ERP-BaseApp/src/StudentRegistration/ERP.StudentRegistration.DataService/obj/Debug/net8.0/ERP.StudentRegistration.DataService.AssemblyInfo.cs
+++ ERP-BaseApp/src/StudentRegistration/ERP.StudentRegistration.DataService/obj/Debug/net8.0/ERP.StudentRegistration.DataService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.StudentRegistration.DataService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc75ddf4861784beafc2df8d7d9acacc05e7966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.StudentRegistration.DataService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.StudentRegistration.DataService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
