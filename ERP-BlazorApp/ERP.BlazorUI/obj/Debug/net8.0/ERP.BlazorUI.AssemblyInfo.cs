ed8dc6ce46f9c8467f8344d83c0db64155f42f9b
Sahaswari <smsahaswari@gmail.com>
2024-04-02 14:15:56 +0530

    Create Add LabEquipment Part1

--- ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
+++ ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35998648a497c23b5fc0416fd2d42eef7bdb6ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
