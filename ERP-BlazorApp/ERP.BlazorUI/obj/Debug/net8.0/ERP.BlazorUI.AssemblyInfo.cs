3c341c1d1bc7f646ee5c91d29f599fbe87eb599e
riyajidevindu <riyajidevindu@gmail.com>
2024-06-11 21:19:09 +0530

    Navigation Bar Changes for Workload

--- ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
+++ ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb97710fd2aeba34642c11bcd8632377fe8002f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bce9651fd1e3a1c9580030611d7b8e041a48f77")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
