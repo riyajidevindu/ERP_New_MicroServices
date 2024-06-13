0d1936af9bc2996c637e830cc38c3beca4ca1d69
riyajidevindu <riyajidevindu@gmail.com>
2024-06-13 22:32:41 +0530

    DTOs for Staff

---
+++ ERP-BlazorApp/Lib/ERP-WorkLoadManagement/DTOs/Staffs/Response/GetStaffResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_WorkLoadManagement.DTOs.Staffs.Response
+{
+    public class GetStaffResponse
+    {
+        public Guid StaffId { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+        public string EmployeeId { get; set; } = string.Empty;
+        public string EmployeeType { get; set; } = string.Empty;
+    }
+}
