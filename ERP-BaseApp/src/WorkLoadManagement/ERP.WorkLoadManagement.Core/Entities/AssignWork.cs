a7bbe5980072dc045c1b9580363db96762be0ac8
riyajidevindu <riyajidevindu@gmail.com>
2024-06-18 21:29:43 +0530

    Models and DTO's for Assign work

---
+++ ERP-BaseApp/src/WorkLoadManagement/ERP.WorkLoadManagement.Core/Entities/AssignWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.WorkLoadManagement.Core.Entities
+{
+    public class AssignWork : BaseEntity
+    {
+        public Guid WorkId { get; set; }
+        public Guid StaffId { get; set; }
+        public string Duration { get; set; }
+        public bool IsRejected { get; set; } 
+        public DateTime AssignedDate { get; set; }
+        public Guid AssignByUserId { get; set; }
+    }
+}
