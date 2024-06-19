4abd04a50e85359c24cb53376e1e5bb887d88e09
riyajidevindu <riyajidevindu@gmail.com>
2024-06-20 00:43:30 +0530

    DTO changes for assign work

--- ERP-BlazorApp/Lib/ERP-WorkLoadManagement/DTOs/AssignWorks/Request/UpdateAssignWorkRequest.cs
+++ ERP-BlazorApp/Lib/ERP-WorkLoadManagement/DTOs/AssignWorks/Request/UpdateAssignWorkRequest.cs
@@ -13,7 +13,7 @@ namespace ERP_WorkLoadManagement.DTOs.AssignWorks.Request
         public Guid? StaffId { get; set; }
         public string Duration { get; set; } = string.Empty;
         public bool IsRejected { get; set; } = false;
-        public DateTime AssignedDate { get; set; } = DateTime.Now;
+        //public DateTime AssignedDate { get; set; } = DateTime.Now;
         public Guid? AssignByUserId { get; set; }
     }
 }
