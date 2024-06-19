d2d3cb3a3f6afc1c1f2b0bbf475885094eeb4572
riyajidevindu <riyajidevindu@gmail.com>
2024-06-20 00:00:15 +0530

    Connect API's with staff UI

--- ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
+++ ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05edcf5ed6e2f563dde10e9f9ab296c92f73a2ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ab150d0b1f5479fc66baf79fd73dae3a1266e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
