9c4b5febc16cf83bac7c5137b267717148f27426
Sahaswari <smsahaswari@gmail.com>
2024-06-20 19:49:34 +0530

    Merge conflict resolve

--- ERP-BaseApp/src/_Shared/ERP.APIGateway/obj/Debug/net8.0/ERP.APIGateway.AssemblyInfo.cs
+++ ERP-BaseApp/src/_Shared/ERP.APIGateway/obj/Debug/net8.0/ERP.APIGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.APIGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2c7c27703233ae652278f2557127130dc97a79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2b8975dfaf23bb31ccbc49e4d0a965c60e4275")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.APIGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.APIGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
