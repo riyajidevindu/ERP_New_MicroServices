7d741fe06fa1b4c2d204e50d6022505abcf6bdda
riyajidevindu <riyajidevindu@gmail.com>
2024-06-26 09:15:52 +0530

    Assign Work tabs removed

--- ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
+++ ERP-BlazorApp/ERP.BlazorUI/obj/Debug/net8.0/ERP.BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1c7064e6fc84c37beffe2f5badc2a5ca6168f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511891cb60e2f9b9603d870a646e588f7a7e0c38")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
